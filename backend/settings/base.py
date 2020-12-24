4f1f15c43c7aacc6cd7f3b12607a409441f6b7bb
Paul Onteri <onteripaul@gmail.com>
2020-12-24 12:20:14 +0300

    static files

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -82,7 +82,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "/media")
 
 # Static
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 STATIC_URL = '/static/'
 
 # Internationalization
